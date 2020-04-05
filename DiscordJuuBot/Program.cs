52e6f70ac3179e9784205643aaafe55ef08a55ee
forgameprogrammer <forgameprogrammer@gmail.com>
2020-04-06 00:15:34 +0300

    Add project files.

---
+++ DiscordJuuBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordJuuBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
